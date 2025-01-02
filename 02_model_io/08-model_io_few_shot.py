d925446c1c93ec55ec4d7b98248247aed3d17610
walle <myeongwoo95@naver.com>
2025-01-02 21:54:38 +0900

    :>

--- 02_model_io/08-model_io_few_shot.py
+++ 02_model_io/08-model_io_few_shot.py
@@ -7,13 +7,13 @@ llm = OpenAI(
 
 examples = [
   {
-    "input": "충청도의 계룡산 전라도의 내장산 강원도의 설악산은 모두 국립 공원이다", # 여기(1) 
-    "output": "충청도의 계룡산, 전라도의 내장산, 강원도의 설악산은 모두 국립 공원이다." # 여기(1) 
+    "input": "충청도의 계룡산 전라도의 내장산 강원도의 설악산은 모두 국립 공원이다", 
+    "output": "충청도의 계룡산, 전라도의 내장산, 강원도의 설악산은 모두 국립 공원이다." 
   },
 ]
 
 prompt = PromptTemplate(
-  input_variables=["input", "output"], # 여기(1) 
+  input_variables=["input", "output"],
   template="입력: {input}\n출력: {output}", 
 )
 
@@ -24,11 +24,11 @@ few_shot_prompt = FewShotPromptTemplate(
   example_prompt=prompt,
   prefix="아래 문장부호가 빠진 입력에 문장부호를 추가하세요. 추가할 수 있는 문장부호는 ',', '.'입니다. 다른 문장부호는 추가하지마세요.",
   suffix="입력: {input_string}\n출력:",
-  input_variables=["input_string"], # 여기(2)
+  input_variables=["input_string"], 
 )
 
 formatted_prompt = few_shot_prompt.format(
-  input_string="집을 보러 가면 그 집이 내가 원하느 조건에 맞는지 살기에 편한지 망가진 곳은 없는지 확인해야 한다" # 여기(2)
+  input_string="집을 보러 가면 그 집이 내가 원하느 조건에 맞는지 살기에 편한지 망가진 곳은 없는지 확인해야 한다"
 )
 
 result = llm.predict(formatted_prompt)
--- 02_model_io/09-list_output_parser.py
+++
@@ -1,6 +0,0 @@
-from langchain import PromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.output_parsers import DatetimeOutputParser
-from langchain.schema import HumanMessage
-
-output_parser = DatetimeOutputParser() # 초기화
